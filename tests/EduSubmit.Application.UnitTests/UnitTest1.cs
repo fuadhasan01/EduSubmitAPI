4e3304efe73ba202cc466b3b0d8c41d1c1c06487
Fuad Hasan <labib.knc@gmail.com>
2026-08-08 01:30:33 +0600

    Initial project structure

---
+++ tests/EduSubmit.Application.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace EduSubmit.Application.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/EduSubmit.Domain.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace EduSubmit.Domain.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
